e500836f16beb95d632dacb225d59c0eae8b046b
Daniel Gibson <metalcaedes@gmail.com>
2025-05-16 14:56:45 +0200

    Set "noclipmodel" spawn arg of particles to true
    If this isn't set, it will try to load a collisionmodel later in
    idEntity::InitDefaultPhysics() (and fail, because it doesn't exist).
    This is done every time a particle is spawned that way, which also
    happens frequently during runtime, so this saves quite some pointless
    work (including trying to open particleName.cm at several different
    locations).
    
    This was especially noticeable when setting `developer 1` in
    vig_surveil.map which then spammed the console with messages like
    "idFileSystem::OpenOSFile: could not open file due to err 2:
     /home/alexander/.config/skindeep/base/spaceparticles64_forcezerog.cm"

--- neo/d3xp/Game_local.cpp
+++ neo/d3xp/Game_local.cpp
@@ -6857,6 +6857,7 @@ idEntity * idGameLocal::DoParticle(const char *particleName, idVec3 position, id
 	args.SetBool("start_off", true);
 	args.SetBool("airlessGravity", airlessGravity);
 	args.SetVector("_color", color);
+	args.SetBool("noclipmodel", true); // DG: the particles don't have a clipmodel and don't need one either
 	idleSmoke = static_cast<idFuncEmitter *>(gameLocal.SpawnEntityType(idFuncEmitter::Type, &args));	
 	if (idleSmoke)
 	{
