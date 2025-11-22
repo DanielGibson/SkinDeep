052302f0c2b1cb4d32abcc7e6540b2c822bb2f9d
Daniel Gibson <metalcaedes@gmail.com>
2025-11-22 03:13:00 +0100

    Update03: Push out any objects inside the cargohide
    This addresses the issue where a key inside the cargohide (ie laundry
     machine) cannot be frobbed, due to the cargohide frob box taking
    priority.

--- neo/d3xp/bc_cargohide.cpp
+++ neo/d3xp/bc_cargohide.cpp
@@ -8,6 +8,8 @@
 //#include "Moveable.h"
 //#include "trigger.h"
 
+#include "bc_key.h"
+
 #include "bc_cargohide.h"
 
 
@@ -68,6 +70,70 @@ bool idCargohide::DoFrob(int index, idEntity * frobber)
 	isFrobbable = false;
 	StartSound("snd_enter", SND_CHANNEL_ANY, 0, false, NULL);
 	gameLocal.GetLocalPlayer()->SetHideState(this, spawnArgs.GetInt("cargotype", "0")); //Tell physics system to move player into the hidey spot.
+
+
+	//BC 10-02-2025: SD 699. Push out any objects inside the cargohide. This addresses the issue where
+	//a key inside the cargohide (ie laundry machine) cannot be frobbed, due to the cargohide frob box
+	//taking priority.
+	PushObjects();
+
+
 	return true;
 }
 
+
+void idCargohide::PushObjects()
+{
+	if (spawnArgs.GetInt("cargotype") != 2) //only for the laundry cargohide
+		return;
+
+	idEntity* entityList[MAX_GENTITIES];
+	int			listedEntities, i;
+
+	//detect if there's stuff waiting to be trashed...
+	listedEntities = gameLocal.EntitiesWithinAbsBoundingbox(GetPhysics()->GetAbsBounds(), entityList, MAX_GENTITIES);
+
+	if (listedEntities > 0)
+	{
+		for (i = 0; i < listedEntities; i++)
+		{
+			idEntity* ent = entityList[i];
+
+			if (!ent)
+			{
+				continue;
+			}
+
+			if (ent == this || ent->IsHidden())
+			{
+				continue;
+			}
+
+			if (gameLocal.GetLocalPlayer()->GetCarryable() != NULL)
+			{
+				if (gameLocal.GetLocalPlayer()->GetCarryable() == ent)
+				{
+					continue;
+				}
+			}
+
+			if (ent->IsType(idSecurityKey::Type))
+			{
+				//Move object to the origin point, so that it's guaranteed to exit the space
+				ent->SetOrigin(GetPhysics()->GetOrigin());
+
+
+				//Throw the object.
+				#define THROWAMOUNT_FORWARD 256
+				#define THROWAMOUNT_UP 32
+				idVec3 forward;
+				this->GetPhysics()->GetAxis().ToAngles().ToVectors(&forward, NULL, NULL);
+
+				idVec3 throwVelocity = (forward * THROWAMOUNT_FORWARD) + (idVec3(0,0, THROWAMOUNT_UP));
+				ent->GetPhysics()->SetLinearVelocity(throwVelocity);
+			}
+
+			
+		}
+	}
+}
